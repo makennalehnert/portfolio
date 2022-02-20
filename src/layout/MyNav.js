0129a0e7f069afc2b49072c5f43df092e00faf5c
Makenna Lehnert <makennaiklehnert@gmail.com>
2022-02-19 19:28:08 -0800

    made whole nav circle clickable

--- src/layout/MyNav.js
+++ src/layout/MyNav.js
@@ -6,21 +6,27 @@ export default function MyNav() {
     <div id="wholeNav">
       <div className="navcontainer">
         <div className="nav">
+          <a className="balllink" href="/">
           <div className="navballs">
             <a href="/" className="links">
               Home
             </a>
           </div>
+          </a>
+          <a className="balllink" href="/aboutme">
           <div className="navballs">
             <a href="/aboutme" className="links">
               About Me
             </a>
           </div>
+          </a>
+          <a className="balllink" href="/projects">
           <div className="navballs">
             <a href="/projects" className="links">
               Projects
             </a>
           </div>
+          </a>
         </div>
       </div>
       <hr></hr>
